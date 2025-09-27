ec1f7b174ceb612758ec5568072ae81f80b7ad2f
Vendogram Ai Staff  - Ryan <achref@vendogram.ai>
2025-09-27 12:17:18 -0400

    Removed extra random imports

--- Easy-ones/Random-Number.py
+++ Easy-ones/Random-Number.py
@@ -1,3 +1,3 @@
-from random import randint, random, choice, uniform, randrange, sample, shuffle, choices, seed, getrandbits, randbytes, SystemRandom
+from random import randint
 for i in range(1000):
     print(randint(1, 10))
